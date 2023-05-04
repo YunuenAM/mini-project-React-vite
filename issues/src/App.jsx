c93eddc5fd01b4690b46cc47167dba213bcf0c9d
Yunuen AM <yunameza@outlook.com>
2023-05-04 10:56:18 -0600

    Feat: SearchBar added

--- issues/src/App.jsx
+++ issues/src/App.jsx
@@ -7,13 +7,23 @@ function App () {
   const [issues, setIssues] = useState([])
 
   const sendSearch = (search) => {
-    fetch('https://api.github.com/repos/facebook/react/issues')
-      .then((res) => res.json())
-      .then((results) => {
-        const data = results.items
-        setIssues(data)
+    if (issues.length > 0) {
+      const filteredIssues = issues.filter((issue) => {
+        return issue.title.includes(search) || issue.body.includes(search) || issue.labels.some((label) => label.name.includes(search))
       })
-      .catch((err) => console.log(err))
+      setIssues(filteredIssues)
+    } else {
+      fetch(`https://api.github.com/search/issues?q=${search}+repo:facebook/react`)
+        .then((res) => res.json())
+        .then((results) => {
+          const data = results.items
+          const filteredIssues = data.filter((issue) => {
+            return issue.id.includes(search) || issue.user.login.includes(search) || issue.html_url.includes((search))
+          })
+          setIssues(filteredIssues)
+        })
+        .catch((err) => console.log(err))
+    }
   }
 
   useEffect(() => {
@@ -39,43 +49,3 @@ function App () {
 }
 
 export default App
-
-// import { useState, useEffect } from 'react'
-// import IssueList from './components/IssueList'
-// import SearchBar from './components/SearchBar'
-
-// function App () {
-//   const [issues, setIssues] = useState([])
-
-//   const sendSearch = (search) => {
-//     fetch('https://api.github.com/repos/facebook/react/issues')
-//       .then(res => res.json())
-//       .then(results => {
-//         const { data } = results
-//         setIssues(data)
-//       })
-//       .catch(err => console.log(err))
-//   }
-
-//   useEffect(() => {
-//     fetch('https://api.github.com/repos/facebook/react/issues')
-//       .then(res => res.json())
-//       .then(results => {
-//         const { data } = results
-//         setIssues(data)
-//       }).catch(err => console.log(err))
-//   }, [])
-
-//   return (
-//     <div className='App'>
-//       <SearchBar handleSearch={sendSearch} />
-//       <div className='grid-cards'>
-//         {issues.map(issue => (
-//           <IssueList key={issue.id} url={issue.html_url} />
-//         ))}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App
