110708a946961c534a5b6f1582f883c17968624b
Yunuen AM <yunameza@outlook.com>
2023-05-03 21:57:36 -0600

    Feat: SearchBar added

--- issues/src/App.jsx
+++ issues/src/App.jsx
@@ -1,12 +1,81 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import IssueList from './components/IssueList'
+import SearchBar from './components/SearchBar'
+import './App.css'
 
 function App () {
+  const [issues, setIssues] = useState([])
+
+  const sendSearch = (search) => {
+    fetch('https://api.github.com/repos/facebook/react/issues')
+      .then((res) => res.json())
+      .then((results) => {
+        const data = results.items
+        setIssues(data)
+      })
+      .catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    fetch('https://api.github.com/repos/facebook/react/issues')
+      .then((res) => res.json())
+      .then((results) => {
+        const data = results
+        setIssues(data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <div className='App'>
-      <IssueList />
+      <SearchBar handleSearch={sendSearch} />
+      <div className='grid-cards'>
+        {issues.map((issue) => (
+          <IssueList key={issue.id} url={issue.html_url} />
+        ))}
+      </div>
     </div>
   )
 }
 
 export default App
+
+// import { useState, useEffect } from 'react'
+// import IssueList from './components/IssueList'
+// import SearchBar from './components/SearchBar'
+
+// function App () {
+//   const [issues, setIssues] = useState([])
+
+//   const sendSearch = (search) => {
+//     fetch('https://api.github.com/repos/facebook/react/issues')
+//       .then(res => res.json())
+//       .then(results => {
+//         const { data } = results
+//         setIssues(data)
+//       })
+//       .catch(err => console.log(err))
+//   }
+
+//   useEffect(() => {
+//     fetch('https://api.github.com/repos/facebook/react/issues')
+//       .then(res => res.json())
+//       .then(results => {
+//         const { data } = results
+//         setIssues(data)
+//       }).catch(err => console.log(err))
+//   }, [])
+
+//   return (
+//     <div className='App'>
+//       <SearchBar handleSearch={sendSearch} />
+//       <div className='grid-cards'>
+//         {issues.map(issue => (
+//           <IssueList key={issue.id} url={issue.html_url} />
+//         ))}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
